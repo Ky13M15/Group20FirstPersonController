a0ea286b7a45e29fb7b2e3ef02b9ffb59200a55a
Tebo0o <sep.t.sepato@gmail.com>
2025-09-23 20:27:00 +0200

    Explosion Change in line

--- FPC- (A void)/Assets/scripts/Explosions.cs
+++ FPC- (A void)/Assets/scripts/Explosions.cs
@@ -2,10 +2,6 @@ using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
-<<<<<<< Updated upstream
-=======
-using System.Collections;
->>>>>>> Stashed changes
 
 //Title: How to make a METEOR SHOWER for your game - Unity - Blender - Visual effects
 //Author: Stick Wizard
